4b6350fbb161d5f459f1eb4061ea474b99c767d0
Italo204 <italoosantos32@gmail.com>
2024-08-19 21:33:14 -0300

    Creating EasyTests folder

---
+++ EasyTests/1.TwoSums/TwoSums.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class TwoSums
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for(int i = 0; i < nums.lenght; i ++)
+            {
+                for(int j =0; j < nums.lenght; j++)
+                {
+                    if(nums[i] + nums[j] = target)
+                    {
+                        int[] result = nums[i];
+                        result = nums[j]
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+}
