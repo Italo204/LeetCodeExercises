f53f12b2e9ebca05709ae25f366a5026cd6cd2ae
Italo204 <italoosantos32@gmail.com>
2024-08-19 22:33:24 -0300

    PalindromeNumber

---
+++ EasyTests/9.PalindromeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Test
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string stringNumber = Convert.ToString(x);
+            char[] arrayString = stringNumber.ToCharArray();
+            List<char> reverse = new List<char>();
+            for(int i = arrayString.Length - 1; i >= 0; i--)
+            {
+                reverse.Add(arrayString[i]); 
+            } 
+            String stringReverse = string.Join("", reverse);
+            if(!stringReverse.Equals(stringNumber))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            bool result = solution.IsPalindrome(121);
+            Console.WriteLine(result);
+        }
+    }
+}
+
